fad1fa8f3f175aee2df2bf7dde45d62f5448ee46
AlfieriChou <alfierichou@gmail.com>
2024-09-07 23:06:44 +0800

    feat: read lottery xlsx

--- libs/lottery.py
+++ libs/lottery.py
@@ -1,8 +1,33 @@
 from bs4 import BeautifulSoup
-from libs.html import get_html
 import urllib.request
+import pandas as pd
+import re
+import datetime
+
+from libs.html import get_html
+from libs.area import area_dict, area_list
 
 base_url = 'https://zhs.mof.gov.cn'
+
+
+def Not_none(n):
+  if n != None:
+    return n
+
+
+def get_title_exp_time(link_title):
+  time = list(
+    filter(
+      Not_none,
+      [
+        re.search(r'\d{4}年\d{1}月', link_title),
+        re.search(r'\d{4}年\d{2}月', link_title),
+        re.search(r'\d{4}年第\d{1}期', link_title),
+      ],
+    )
+  )[0]
+  return time
+
 
 
 def get_lottery_list_url(page):
@@ -13,7 +38,7 @@ def get_lottery_list_url(page):
 
 
 def download_lottery_by_page(page):
-  download_list = []
+  download_dict = {}
   lottery_url = get_lottery_list_url(page)
   soup = BeautifulSoup(get_html(lottery_url), 'html.parser')
   ul_list = soup.find_all('ul', class_='liBox')
@@ -25,6 +50,8 @@ def download_lottery_by_page(page):
       title = a.get('title').encode('iso-8859-1').decode('UTF-8')
       if '彩票销售情况' not in title:
         continue
+      time = get_title_exp_time(title)
+      series_name = time.group()
       # 2017年3月的数据结构不同，td->ol->a
       if '2017年3月份全国彩票销售情况' in title:
         soup = BeautifulSoup(get_html(href), 'html.parser')
@@ -35,7 +62,7 @@ def download_lottery_by_page(page):
         print(href, title, download_url)
         download_path = 'xlsx/' + download_href[2:]
         urllib.request.urlretrieve(download_url, download_path)
-        download_list.append(download_path)
+        download_dict[series_name] = download_path
         continue
       soup = BeautifulSoup(
         get_html(base_url + '/zonghexinxi/' + href[2:]), 'html.parser'
@@ -51,6 +78,25 @@ def download_lottery_by_page(page):
       print(href, title, download_url)
       download_path = 'xlsx/' + download_href[2:]
       urllib.request.urlretrieve(download_url, download_path)
-      download_list.append(download_path)
+      download_dict[series_name] = download_path
 
-  return download_list
+  return download_dict
+
+def read_lottery_xlsx(series_name, xlsx_path):
+  # 暂不支持解析xls文件
+  if xlsx_path.endswith('.xls') is True:
+    return
+  list = []
+  dfs = pd.read_excel(xlsx_path, sheet_name=None)
+  for sheet_name, value in dfs.items():
+    if '各地区彩票销售情况' in sheet_name or 'Sheet3' in sheet_name:
+      for index, row in value.iterrows():
+        lottery_list = row.to_list()
+        if lottery_list[0] not in area_list:
+          continue
+        lottery_list.insert(0, area_dict[lottery_list[0]])
+        lottery_list.insert(0, series_name)
+        print(xlsx_path, index, lottery_list)
+        list.append(lottery_list)
+
+  return list
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from libs.lottery import download_lottery_by_page
+import time
+
+from libs.lottery import download_lottery_by_page, read_lottery_xlsx
 
 total_page = 29
 page = 1
 
 while page < total_page:
-  download_list = download_lottery_by_page(page)
-  print('[DOWNLOAD-LIST]: ', page, download_list)
+  download_dict = download_lottery_by_page(page)
+  print('[DOWNLOAD-LIST]: ', page, download_dict)
+  for series_name, xlsx_path in download_dict.items():
+    read_lottery_xlsx(series_name, xlsx_path)
+    time.sleep(1)
+  time.sleep(5)
   page += 1
